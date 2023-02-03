5eae7206cd4cfe06f7b086d9bb6bf61a621bb00b
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-02-03 11:38:23 -0700

    Passed parameters to callbacks.

--- timer.js
+++ timer.js
@@ -14,10 +14,10 @@ class Timer{
     };
     start = () => {
         if(this.onStart){
-            this.onStart();
+            this.onStart(this.timeRemaining);
         }
         this.tick();
-        this.interval = setInterval(this.tick,1000);
+        this.interval = setInterval(this.tick,50);
     };
 
     pause = () => {
@@ -31,9 +31,9 @@ class Timer{
                 this.onComplete();
             };
         } else {
-            this.timeRemaining = this.timeRemaining -1;
+            this.timeRemaining = this.timeRemaining - 0.05;
             if(this.onTick){
-                this.onTick();
+                this.onTick(this.timeRemaining);
             };
         };
     };
