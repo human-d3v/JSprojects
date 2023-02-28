c9eb0d0af803a1985bd0a27462d9bb8e11d49be7
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-02-28 10:47:57 -0700

    Added functionality to dropdown menu.

--- movieComp.js
+++ movieComp.js
@@ -1,6 +1,6 @@
 const apiKey = 'f5b2346e';
-let searchTerm = 'Llewyn Davis';
 const url = `http://www.omdbapi.com/`;
+
 
 const fetchData = async (searchTerm) => {
     const resp = await axios.get(url, {
@@ -9,24 +9,65 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
-    console.log(resp.data);
+    if(resp.data.Error) {
+        return [];
+    };
+    return resp.data.Search;
 };
 
-const input = document.querySelector('#searchBox');
-const debounce = (func) => {
-    return () => {
+//base HTML to access the dropdown menu
+    const root = document.querySelector('.autocomplete');
+    root.innerHTML = `
+        <label><b>Search for a Movie</b></label>
+        <input class="input" />
+        <div class="dropdown">
+            <div class="dropdown-menu">
+                <div class="dropdown-content results"></div>
+            </div>
+        </div>
+    `;
+    // selectors
+    const input = document.querySelector('.input');
+    const dropdown = document.querySelector('.dropdown');
+    const resultsWrapper = document.querySelector('.results');
 
-    };
-}
 
-let timeoutId;
-const onInput = (event) => {
-    if (timeoutId) { //first loop will return undefined and bypass this logic. 
-        clearTimeout(timeoutId);
-    };
-    timeoutId = setTimeout(() => {
-        fetchData(event.target.value);
+//user input
+    const onInput = debounce(async event => {
+        const movies = await fetchData(event.target.value);
+
+        if(!movies.length) { //if user removes the search string, remove dropdown menu
+            dropdown.classList.remove('is-active');
+            return;
+        };
+
+        resultsWrapper.innerHTML = '';
+        dropdown.classList.add('is-active'); //show dropdown
+
+        for(let movie of movies){ //iterate over options returned from fetchData()
+            const option= document.createElement('a'); 
+            const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster; //handle broken images from API
+
+            option.addEventListener('click', () => { //when an option is clicked from the dropdown menu, update the input text to match the title and remove dropdown
+                dropdown.classList.remove('is-active');
+                input.value = movie.Title;
+            })
+
+            option.classList.add('dropdown-item');
+            option.innerHTML = `
+                <img src="${imgSrc}" />
+                ${movie.Title}
+            `;
+
+            resultsWrapper.appendChild(option);
+        }
     },1000);
-};
 
-input.addEventListener('input',onInput);
+//event listeners
+    input.addEventListener('input',onInput);
+
+    document.addEventListener('click', event => {
+        if(!root.contains(event.target)){
+            dropdown.classList.remove('is-active');
+        };
+    })
