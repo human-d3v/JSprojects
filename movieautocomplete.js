351002e84e062c25b75dc816a1c917c6e22fe9c2
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-03-07 10:59:36 -0700

    Created barebones framework for callback behavior.

---
+++ movieautocomplete.js
@@ -0,0 +1,12 @@
+/*this file will contain an function that will accept an object with the following callbacks:
+    {
+        fetchData(), //specific function for pulling data from an api
+        renderOption(), //function that will know how to render the values returned from API
+        onOptionSelect(), //function that will be invoked when user clicks an option rendered as specified above
+        root //the element that the autocomplete will ultimately render on selection
+    }
+*/
+
+const createAutoComplete = (config) => {
+    
+};
