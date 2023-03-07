d7dea7c4402d12c501a003099e67011dbe7adb77
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-03-07 10:56:35 -0700

    Added HTML Rendering. Refactor Required.
    Both onInput() and onMovieSelect() are touching too many global variables. I need to refactor them to sequester them to specific functionality.

--- movieComp.js
+++ movieComp.js
@@ -51,6 +51,7 @@ const fetchData = async (searchTerm) => {
             option.addEventListener('click', () => { //when an option is clicked from the dropdown menu, update the input text to match the title and remove dropdown
                 dropdown.classList.remove('is-active');
                 input.value = movie.Title;
+                onMovieSelect(movie)
             })
 
             option.classList.add('dropdown-item');
@@ -63,11 +64,62 @@ const fetchData = async (searchTerm) => {
         }
     },1000);
 
+//user select 
+    const onMovieSelect = async movie => { //fetch one movie
+        const resp = await axios.get(url,{
+            params: {
+                apikey: apiKey,
+                i: movie.imdbID
+            }
+        });
+       
+        document.querySelector('#summary').innerHTML = movieTemplate(resp.data);
+    };
+
+    movieTemplate = movieDetail => {
+        return `
+            <article class="media">
+                <figure class="media-left">
+                    <p class="image">
+                        <img src="${movieDetail.Poster}" />
+                    </p>
+                </figure>
+                <div class="media-content">
+                    <div class="content">
+                        <h1>${movieDetail.Title}</h1>
+                        <h4>${movieDetail.Genre}</h4>
+                        <p>${movieDetail.Plot}</p>
+                    </div>
+                </div>
+            </article>
+            <article class="notification is-primary">
+                <p class="title">${movieDetail.Awards}</p>
+                <p class="subtitle">Awards</p>
+            </article>
+            <article class="notification is-primary">
+                <p class="title">${movieDetail.BoxOffice}</p>
+                <p class="subtitle">Box Office</p>
+            </article>
+            <article class="notification is-primary">
+                <p class="title">${movieDetail.Metascore}</p>
+                <p class="subtitle">Metascore</p>
+            </article>
+            <article class="notification is-primary">
+                <p class="title">${movieDetail.imdbRating}</p>
+                <p class="subtitle">IMDB Rating</p>
+            </article>
+            <article class="notification is-primary">
+                <p class="title">${movieDetail.imdbVotes}</p>
+                <p class="subtitle">IMDB Votes</p>
+            </article>
+        `;
+    }
+
 //event listeners
     input.addEventListener('input',onInput);
 
-    document.addEventListener('click', event => {
+    document.addEventListener('click', event => { //hide dropdown if user clicks away from menu
         if(!root.contains(event.target)){
             dropdown.classList.remove('is-active');
         };
-    })
+    }); 
