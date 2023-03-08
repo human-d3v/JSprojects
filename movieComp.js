580c643a8f26ab550feb7d61637cb79b64d077e9
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-03-08 14:44:25 -0700

    Moved first fetchData to the autocomplete object.
    Additional refactor still required to sequester the onMovieSelect data.

--- movieComp.js
+++ movieComp.js
@@ -2,7 +2,23 @@ const apiKey = 'f5b2346e';
 const url = `http://www.omdbapi.com/`;
 
 
-const fetchData = async (searchTerm) => {
+
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie){
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster; //handle broken images from API
+      return `
+          <img src="${imgSrc}" />
+              ${movie.Title} (${movie.Year})
+      `;
+  },
+  onOptionSelect(movie){
+      return onMovieSelect(movie);
+  },
+  inputValue(movie){
+      return movie.Title;
+  },
+  async fetchData(searchTerm){
     const resp = await axios.get(url, {
         params: {
             apikey: apiKey,
@@ -13,56 +29,9 @@ const fetchData = async (searchTerm) => {
         return [];
     };
     return resp.data.Search;
-};
+  }
+});
 
-//base HTML to access the dropdown menu
-    const root = document.querySelector('.autocomplete');
-    root.innerHTML = `
-        <label><b>Search for a Movie</b></label>
-        <input class="input" />
-        <div class="dropdown">
-            <div class="dropdown-menu">
-                <div class="dropdown-content results"></div>
-            </div>
-        </div>
-    `;
-    // selectors
-    const input = document.querySelector('.input');
-    const dropdown = document.querySelector('.dropdown');
-    const resultsWrapper = document.querySelector('.results');
-
-
-//user input
-    const onInput = debounce(async event => {
-        const movies = await fetchData(event.target.value);
-
-        if(!movies.length) { //if user removes the search string, remove dropdown menu
-            dropdown.classList.remove('is-active');
-            return;
-        };
-
-        resultsWrapper.innerHTML = '';
-        dropdown.classList.add('is-active'); //show dropdown
-
-        for(let movie of movies){ //iterate over options returned from fetchData()
-            const option= document.createElement('a'); 
-            const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster; //handle broken images from API
-
-            option.addEventListener('click', () => { //when an option is clicked from the dropdown menu, update the input text to match the title and remove dropdown
-                dropdown.classList.remove('is-active');
-                input.value = movie.Title;
-                onMovieSelect(movie)
-            })
-
-            option.classList.add('dropdown-item');
-            option.innerHTML = `
-                <img src="${imgSrc}" />
-                ${movie.Title}
-            `;
-
-            resultsWrapper.appendChild(option);
-        }
-    },1000);
 
 //user select 
     const onMovieSelect = async movie => { //fetch one movie
@@ -114,12 +83,3 @@ const fetchData = async (searchTerm) => {
             </article>
         `;
     }
-
-//event listeners
-    input.addEventListener('input',onInput);
-
-    document.addEventListener('click', event => { //hide dropdown if user clicks away from menu
-        if(!root.contains(event.target)){
-            dropdown.classList.remove('is-active');
-        };
-    }); 
