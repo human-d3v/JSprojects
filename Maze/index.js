2b4e934f16ed96732dcee0809391061ee85e0a19
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-03-15 14:42:38 -0600

    Initial Upload for files.
    This app uses the matterjs engine to create a simple maze algorithm.

---
+++ Maze/index.js
@@ -0,0 +1,45 @@
+//This file will use the matterjs library to create a maze, render an object like a ball and map keystrokes (W,S,A,D) to movements within the matterjs engine.
+//I might also change the circle to a clipart corn, because it'd be funny to call this app 'The Maize'
+
+const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter; //destructuring from matterjs library
+
+const width = 800;
+const height = 600;
+const engine = Engine.create();
+const {world} = engine; //engine is created from world
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    // wireframes:false,
+    width: width, 
+    height: height
+  }
+});
+
+Render.run(render);
+Runner.run(Runner.create(),engine);
+
+World.add(world, MouseConstraint.create(engine, {
+  mouse: Mouse.create(render.canvas)
+}));
+
+//Walls
+const walls = [
+  Bodies.rectangle(400,0,800,40,{isStatic:true}),
+  Bodies.rectangle(400,600,800,40,{isStatic:true}),
+  Bodies.rectangle(0,300,40,800,{isStatic:true}),
+  Bodies.rectangle(800,300,40,800,{isStatic:true})
+];
+
+World.add(world,walls);
+
+//Random Shapes to fill world
+
+for (let i = 0; i<20; i++){
+  if(Math.random() > 0.5){
+    World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height,50,50));
+  } else {
+    World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 31));
+  }
+};
