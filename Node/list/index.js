7c44327482f39b1139e2c2c625f9cfdea6c2ff2d
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-05-26 12:07:26 -0600

    Major Cleanup
    Major Cleanup

--- Node/list/index.js
+++ Node/list/index.js
@@ -1,99 +1,26 @@
+/this file acts like the 'ls' command in linux/unix systems.
 #! C:\Users\bryar.topham\scoop\apps\nodejs\current\node
 
-const exp = require('constants');
 const fs = require('fs');
-//const util = require('util'); //this will import promisify for s3B
+const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
 
-
-/*
-	because of the asynchronous nature of the program reaching out to the 
-	hard drive, the the array returned by this solution is often populated by 
-	whatever the fastest write-out was, which can change upon each execution
-*/
-//function solutionOne(filenames) { //this is a bad solution 
-//	for (let f of filenames) {
-//		fs.fstat(f, (err, stats) => {
-//			if (err) {
-//				throw new Error(err);
-	//		}
-	//		console.log(f, stats.isFile());
-		//});
-	//};
-//}
-
-
-
-/*
-	this solution sticks with the callback theme of the above solution, but, 
-	like the first solution, it relies entirely on callbacks.
-*/
-//function solutionTwo(filenames) { //sticking with callback
-//	const expected = Array(filename.length).fill(null); //empty array 
-//	for (let f of filenames) {
-//		const idx = filenames.indexOf(f);
-//
-//		fs.fstat(f, (err, stats) => {
-//			if (err) {
-//				throw new Error(err);
-//			}
-//			expected[idx] = stats;
-//			const ready = expected.every(() => {
-//				return stats;
-//			});
-//			if (ready) {
-//				expected.forEach((stat, index) => {
-//					console.log(filenames[index], stat.isFile());
-//				});
-//			}
-//		});
-//	};
-//};
-
-
-/*
-	this solution relies on async & await to resolve the read-write promise 
-	to & from harddrive, but still isn't optimized.
-*/
-
-//function solutionThreeA(filenames) {
-//	const lstat = (f) => {
-//		return new Promise((resolve, reject) => {
-//			fs.lstat(f, (err, stats) => {
-//				if (err) {
-//					reject(err);
-//				}
-//				resolve(stats);
-//			});
-//		});
-//	}
-//	for (let f of filenames) {
-//		console.log(lstat(f));
-//	};
-//}
-
-//async function solutionThreeB(filenames) {
-//	const { lstat } = util.promisify(fs.lstat);
-//	for (let f of filenames){
-//		const stats = await lstat(f);
-//		console.log(f, stats.isfile());
-//	};
-//}
-
-//the above implementation is running everything in series, so it's clearly not
-//optimized. This implementation will process calls in parallel
-
-fs.readdir(process.cwd(), async (err, filenames) => {
+const { lstat } = fs.promises;
+const targetDir = process.argv[2] || process.cwd();
+fs.readdir(targetDir, async (err, filenames) => {
 	if(err){
-		throw new Error(err);
+		console.log(err);
 	}
-	const { lstat } = fs.promises;
-	statPromises = filenames.map(f => {
-		return lstat(f);
+	const statPromises = filenames.map(f => {
+		return lstat(path.join(targetDir,f));
 	})
 
 	const allStats = await Promise.all(statPromises);
+
 	for(let stat of allStats){
 		const idx = allStats.indexOf(stat);
-		console.log(filenames[idx], stat.isFile());
+		console.log((stat.isFile() ? chalk.blue(filenames[idx]):
+			chalk.green(filenames[idx])));
 	}
 });
