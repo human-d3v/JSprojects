5eed465d98bd2b19919117402652b681b8c82b3e
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-07-19 14:14:04 -0600

    Added additional methods to UserRepo class and exported instance

--- eCommApp/repos/users.js
+++ eCommApp/repos/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UserRepository{
 	constructor(filename){
@@ -14,16 +15,63 @@ class UserRepository{
 	}
 
 	async getAll() {
-		//open file, read contents and return returns parsed data
-		const contents = await fs.promises.readFile(this.filename, 
-			{encoding: 'utf8'});
-		console.log(contents);
+		return JSON.parse(await fs.promises.readFile(this.filename, 
+			{encoding: 'utf8'}));
+	}
+
+	async create(attrs) {
+		attrs.id = this.randId();
+		const records = await this.getAll();
+		records.push(attrs);
+		this.writeAll(records);
+	}
+	
+	async writeAll(records){
+		await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+	}
+
+	randId(){
+		return crypto.randomBytes(4).toString('hex');
+	}
+
+	async getOne(id){
+		const records = await this.getAll();
+		return records.find(record=>record.id === id);
+	}
+
+	async delete(id){
+		const records = await this.getAll();
+		const filtered = records.filter(r => r.id !== id);
+		await this.writeAll(filtered);
+	}
+
+	async update(id, attrs){
+		const records = await this.getAll();
+		const record = records.find(record=>record.id === id);
+		if(!record){
+			throw new Error(`Record with id ${id} not found`);
+		}
+		Object.assign(record, attrs);
+		await this.writeAll(records);
+	}
+
+	async getOneBy(filters){
+		const records = await this.getAll();
+
+		for(let r of records){ //iterate through array
+			let found = true; //update if not found
+			
+			for(let key in filters){ //iterate through object 
+				if(r[key] !== filters[key]) {
+					found = false;
+				}
+			};
+			if(found){
+				return r;
+			};
+		};
 	}
 }
 
-const test = async () => {
-	const repo = new UserRepository('users.json');
-	await repo.getAll();
-};
-
-test();
+module.exports = new UserRepository('users.json'); 
+//exporting an instance instead of the class itself.
